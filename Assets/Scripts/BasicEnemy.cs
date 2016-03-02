ccbb2e9075d3965590ab418d73d04d464073efbe
Otto Borchert <OttoBorchert@gmail.com>
2016-03-02 10:31:50 -0600

    Updates after class on February 29
    Includes addition of a script that plays a sound when a button is
    pressed and stopped when the button is released. Also added explosions
    via Unity's ParticleEffects package

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemy : MonoBehaviour {
+
+	//The prefab to use as our explosion graphic
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		//Create the explosion and destroy the enemy when anything collides with it.
+		Instantiate (explosion, this.transform.position, Quaternion.identity);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+	//Stores the AudioClip we want to play
+	AudioSource myAudioSource;
+
+
+	// Use this for initialization
+	void Start () {
+		//Load the AudioSource...
+		myAudioSource = GetComponent<AudioSource> ();
+
+		//...and make sure it doesn't start right away
+		myAudioSource.playOnAwake = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//When you press P, start the song
+		if (Input.GetKeyDown (KeyCode.P)) {
+			myAudioSource.Play ();
+		}
+		//And when you release P, pause the song
+		if (Input.GetKeyUp (KeyCode.P)) {
+			myAudioSource.Pause ();
+		}
+	}
+}
